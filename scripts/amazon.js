43e4576ee675250a68dcf66a969fc08afde8afd1
Themiya <themiyakaluarachchi@gmail.com>
2025-04-30 00:39:02 +0530

    ..

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,1 +1,18 @@
-console.log('hello');       
+const products = 
+[
+  {
+    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+    rating: 
+    {
+      stars: 4.5,
+      count: 87
+    },
+    priceCents: 1090
+  },
+  {
+    image: 'images/products/intermediate-composite-basketball.jpg',
+    name: 'Intermediate Size Basketball',
+    rating:
+  }
+];    
