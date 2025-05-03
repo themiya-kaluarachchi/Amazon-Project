756499815c56c7bd7783d04ad8012bf34c029981
Themiya <themiyakaluarachchi@gmail.com>
2025-05-04 04:07:09 +0530

    Save cart in localStorage

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -107,7 +107,7 @@ document.querySelectorAll('.js-delete-link').forEach((link) =>
   {
     const productId = link.dataset.productId;
     removeFromCart(productId);
-    console.log(cart);
+    //console.log(cart);
 
     const container =  document.querySelector(
       `.js-cart-item-container-${productId}`
