8ca99f7951972bb0d7d5b5e0062f8abbdb4297ee
Themiya <themiyakaluarachchi@gmail.com>
2025-05-14 20:12:43 +0530

    ..

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,6 +2,8 @@ import { cart, removeFromCart } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
+hello();
+
 let cartSummaryHTML = '';
 
 cart.forEach((cartItem) => {
