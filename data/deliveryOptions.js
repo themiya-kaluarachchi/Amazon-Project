853a86e762a4969dfb4d2de891a7a5a18019b019
Themiya <themiyakaluarachchi@gmail.com>
2025-05-15 17:52:27 +0800

    Save data and generate HTML for payment summary

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -16,3 +16,17 @@ export const deliveryOptions =
     priceCents: 999
   }
 ];
+
+export function getDeliveryOption(deliveryOptionId)
+{
+  let deliveryOption;
+
+    deliveryOptions.forEach((option) =>
+    {
+      if (option.id === deliveryOptionId) 
+      {
+        deliveryOption = option;
+      } 
+    });
+    return deliveryOption || deliveryOptions[0];  
+}
