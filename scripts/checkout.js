e87d03e1a4d46dd435c7af20520ac31113610dd5
Themiya <themiyakaluarachchi@gmail.com>
2025-05-15 16:31:53 +0800

    ..

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart } from '../data/cart.js';
+import { cart, removeFromCart, updateDeliveryOption } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
@@ -104,7 +104,7 @@ function deliveryOptionsHTML(matchingProduct, cartItem)
 
     html += `
     
-      <div class="delivery-option">
+      <div class="delivery-option js-delivery-option">
         <input type="radio"
           ${isChecked ? 'checked' : ''}
           class="delivery-option-input"
@@ -138,5 +138,13 @@ document.querySelectorAll('.js-delete-link').forEach((link) =>
       `.js-cart-item-container-${productId}`
     );
     container.remove();
+  });
+});
+
+document.querySelectorAll('.js-delivery-option').forEach((element) => 
+{
+  element.addEventListener('click', () => 
+  {
+     updateDeliveryOption(productId, deliveryOptionId);
   });
 });
