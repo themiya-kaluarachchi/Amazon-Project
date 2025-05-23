008de8dea9511554f6fd60f3ee44aae3d2ee5c37
Themiya <themiyakaluarachchi@gmail.com>
2025-05-24 02:04:18 +0530

    Load products from the backend

--- data/products.js
+++ data/products.js
@@ -104,6 +104,7 @@ object3.method();
 */
 
 
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -771,3 +772,35 @@ export const products = [
   }
   return new Product(productDetails);
 });
+*/
+
+
+
+
+export let products = [];
+
+export function loadProducts(func)
+{
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () =>
+  {
+    products = JSON.parse(xhr.response).map((productDetails) => 
+    {
+      if (productDetails.type === 'clothing') 
+      {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+
+    func();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+loadProducts();
